fd70579b51d4a796fcddec7b07f2832cc74b130e
GustaCruz <gustacruz10.gc@gmail.com>
2024-02-27 20:53:23 -0600

    Added crosshairs, improved FPS and added simple UI

--- main.py
+++ main.py
@@ -1,24 +1,106 @@
-# Install Anaconda from the following link: https://www.anaconda.com/download
-# Run and open, use the VSCode launcher, open the terminal and use
-# pip install opencv-contrib-python # some people ask the difference between this and opencv-python
-                                    # and opencv-python contains the main packages wheras the other
-                                    # contains both main modules and contrib/extra modules
-# pip install cvlib # for object detection
-# pip install tensorflow --user
-# use `pip3 install PyObjC`
+#########################################################################
+#                      UCHL - Senior Project                            #
+#                                                                       #
+#   Team: I forgor [Skull emoji]                                        #
+#                                                                       #
+#   Memebers: Gustavo, Nicole, Drew, Dego, Ricardo                      #
+#                                                                       #
+#   Goal: Using CV2 to work on object detection and preform tasks       #
+#                                                                       #
+#   Notes: USE ANACONDA NAVIGATOR AS IT WORKS BEST WITH CV2             #
+#   Link: https://www.anaconda.com/download launch and click VScode     #
+#                                                                       #
+#########################################################################
+'''
+USE PIP TO INSTALL THE FOLLOWING (ONE AT A TIME PLS):
 
-import cv2
-import cvlib as cv
-from cvlib.object_detection import draw_bbox
+pip install opencv-contrib-python
+pip install cvlib # for object detection
+pip install tensorflow --user
+pip3 install PyObjC
 
-video = cv2.VideoCapture(0) # webcam selector
+'''
 
-while True:
-    ret, frame = video.read()
-    bbox, label, conf = cv.detect_common_objects(frame) # charcteristics of obj
-    output_image = draw_bbox(frame, bbox, label, conf) # overlay
+#Imports and Libs
+import cv2, time, cvlib as cv # if you get "error: (-215:Assertion failed)" delete your cvlib folder
+from threading import Thread  # in C:\Users\[USER]\.cvlib
+from cvlib.object_detection import draw_bbox 
+
+#Camera Selector
+class ThreadedCamera(object):
+    def __init__(self, src=0): # The SRC value is the camera selection usually 0-3
+        self.capture = cv2.VideoCapture(src)
+        self.capture.set(cv2.CAP_PROP_BUFFERSIZE, 2)
+        
+        # FPS = 1/X
+        # X = desired FPS, higher FPS results in jittery boxes
+        self.FPS = 1/35 # 30-35 fps seems reasonable
+        self.FPS_MS = int(self.FPS * 1000)
+        
+        # Start frame retrieval thread, at the cost of detection confidence
+        self.thread = Thread(target=self.update, args=())
+        self.thread.daemon = True
+        self.thread.start()
+        
+        
+    def update(self): #ensures camera updates so no frame drops
+        while True:
+            if self.capture.isOpened():
+                (self.status, self.frame) = self.capture.read()
+            time.sleep(self.FPS)
     
-    cv2.imshow("Detection", output_image) #name of the webcam window
+    
+    def detect_and_show_objects(self):
+        if hasattr(self, 'frame'):
+            
+            # Increase sensitivity of detection by confidence %        vvvvvvvvvv     object trainer vvv  makes my pc go vroom
+            bbox, label, conf = cv.detect_common_objects(self.frame, confidence=0.35, model='yolov4-tiny')
+            
+            # Filter only 'bottle' objects for testing purposes because they are "plentiful on my desk"
+            bottle_indices = [i for i, lbl in enumerate(label) if lbl == 'bottle']
+            filtered_bbox = [bbox[i] for i in bottle_indices]
+            filtered_label = [label[i] for i in bottle_indices]
+            filtered_conf = [conf[i] for i in bottle_indices]
+            
+            # Draw bounding boxes and crosshair, considering adding distance measurements but then we got to teach this thing how to count, no thanks.
+            output_image, num_bottles = self.draw_bboxes_with_crosshair(self.frame, filtered_bbox, filtered_label, filtered_conf)
+            
+            # Add text overlay for the number of bottles displayed on the screen (planned on adding 99 bottles of beer joke :D )
+            text = f'Number of bottles: {num_bottles}'
+            cv2.putText(output_image, text, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+            
+            cv2.imshow('frame', output_image)
+            cv2.waitKey(self.FPS_MS)
+    
+    
+    def draw_bboxes_with_crosshair(self, frame, bbox, label, conf):
+        num_bottles = len(bbox)
+        for box, lbl in zip(bbox, label):
+            
+            # Converting label to string as it fucks up on detection otherwise e-e
+            lbl_str = str(lbl)
+            
+            # Calculate center coordinates of the box so the cross hair is accurate, also is nice little hack
+            x_center = int((box[0] + box[2]) / 2)
+            y_center = int((box[1] + box[3]) / 2)
+            
+            # Draw bounding box
+            cv2.rectangle(frame, (box[0], box[1]), (box[2], box[3]), (0, 255, 0), 2)
+            
+            # Draw label on top of the bounding box AS A STRING!!!! can modify this to look fancy!
+            cv2.putText(frame, lbl_str, (box[0], box[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
+            
+            # Drawing crosshair at the center of the box from the lib 
+            cv2.drawMarker(frame, (x_center, y_center), (0, 0, 255), cv2.MARKER_CROSS, 10, 2)
+        
+        return frame, num_bottles # this thing counts bottles yay
 
-    if cv2.waitKey(1) & 0xFF == ord("q"): # to turn off webcam when running press q in code
-        break
+
+# Example usage
+if __name__ == "__main__":
+    threaded_camera = ThreadedCamera(src=0)
+    while True:
+        threaded_camera.detect_and_show_objects()
+        if cv2.waitKey(1) & 0xFF == ord('q'): # this broke but i plan on doing something else down the line
+                                              # dont touch it, it brings me joy :v
+            break
