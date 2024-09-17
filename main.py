0258e87981995422d58faec6e64c8200248240b7
GustaCruz <114456987+GustaCruz@users.noreply.github.com>
2024-09-17 17:53:00 -0500

    Version 3 NO OPEN CV
    Features Working:
    + Camera 30FPS (Always on)
    + 2 point Pinging
    + Distance Calculations
    + Clear Map
    + Set Map Starting Area
    + Empty STAT Page
    + Quit Button Working
    + pip installs

--- main.py
+++ main.py
@@ -5,119 +5,198 @@
 #                                                                       #
 #   Memebers: Gustavo, Nicole, Drew, Diego, Ricardo                     #
 #                                                                       #
-#   Goal: Using CV2 to work on object detection and preform tasks       #
+#   Goal:                                                               #
 #                                                                       #
-#   Notes: USE ANACONDA NAVIGATOR AS IT WORKS BEST WITH CV2             #
-#   Link: https://www.anaconda.com/download launch and click VScode     #
+#   Notes:  UI Version 3: MAIN, MAP, STAT, QUIT are working             #
+#   Link: https://github.com/GustaCruz/openvc2-project/                 #
 #                                                                       #
 #########################################################################
-'''
-USE PIP TO INSTALL THE FOLLOWING (ONE AT A TIME PLS):
+# pip install opencv-python customtkinter tkintermapview geopy requests tk adafruit-circuitpython-rfm69 RPi.GPIO weatherapi pyDatalog sqlalchemy
 
-pip install opencv-contrib-python
-pip install cvlib # for object detection
-pip install tensorflow --user
-pip3 install PyObjC
+import cv2
+import threading
+from tkinter import Frame, Label, Tk
+from PIL import Image, ImageTk
+import customtkinter as ctk
+from tkintermapview import TkinterMapView
+from geopy.distance import geodesic
 
-'''
+ctk.set_appearance_mode("dark")
+ctk.set_default_color_theme("dark-blue")
 
-#Imports and Libs
-import cv2, time, cvlib as cv # if you get "error: (-215:Assertion failed)" delete your cvlib folder
-from threading import Thread  # in C:\Users\[USER]\.cvlib
-from cvlib.object_detection import draw_bbox 
+class App(ctk.CTk):
+    APP_NAME = "SPOTS Pilot System"
+    # GENRAL APP SIZE ==============================================
+    WIDTH = 800
+    HEIGHT = 450
 
-#Camera Selector
-class ThreadedCamera(object):
-    def __init__(self, src=0): # The SRC value is the camera selection usually 0-3
-        self.capture = cv2.VideoCapture(src)
-        self.capture.set(cv2.CAP_PROP_BUFFERSIZE, 2)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.title(App.APP_NAME)
+        self.geometry(f"{App.WIDTH}x{App.HEIGHT}")
+        self.minsize(App.WIDTH, App.HEIGHT)
+        self.protocol("WM_DELETE_WINDOW", self.on_closing)
+
+        self.marker_list = []
+        self.init_ui()
+        self.start_webcam_thread()
+
+    def init_ui(self):
+        self.frame = ctk.CTkFrame(master=self)
+        self.frame.pack(pady=10, padx=6, fill="both", expand=True)
+
+        self.label = ctk.CTkLabel(master=self.frame, text="Drone System", font=("Roboto", 14), height=0)
+        self.label.pack(pady=1, padx=10, side="top", anchor="n")
+
+        self.main_frame = Frame(master=self.frame, width=500, height=380, background="black")
+        self.main_frame.pack(pady=1, padx=8, side="top", anchor="ne", fill="both", expand=True)
+
+        self.webcam_label = Label(self.main_frame)
+        self.webcam_label.pack()
         
-        # FPS = 1/X
-        # X = desired FPS, higher FPS results in jittery boxes
-        self.FPS = 1/35 # 30-35 fps seems reasonable
-        self.FPS_MS = int(self.FPS * 1000)
+# MAP STARTING POINT ==============================================================================
+
+        self.map_widget = TkinterMapView(self.main_frame, corner_radius=0, width=790, height=390)
+        self.map_widget.set_position(29.5783681, -95.1041635)
+        self.map_widget.set_zoom(16)
+        self.map_widget.place_forget()
         
-        # Start frame retrieval thread, at the cost of detection confidence
-        self.thread = Thread(target=self.update, args=())
+# MAP APP SIZE ====================================================================================
+
+        self.map_controls_frame = ctk.CTkFrame(master=self.main_frame, width=150, height=220, corner_radius=0, fg_color=None)
+        self.map_controls_frame.place_forget()
+        self.create_map_controls()
+        self.create_nav_buttons()
+        
+# MAP CONTROLS FOR MAP TAB (MARKERS, BUTTONS, DISTANCE CALCULATIONS) ==============================
+
+    def create_map_controls(self):
+        self.label_marker1 = ctk.CTkLabel(master=self.map_controls_frame, text="Marker 1:", anchor="w")
+        self.label_marker1.pack(pady=(2, 0), padx=(1, 10))
+        self.entry_marker1 = ctk.CTkEntry(master=self.map_controls_frame, placeholder_text="Paste your coords...")
+        self.entry_marker1.pack(pady=(2, 0), padx=(10, 10))
+        self.button_add_marker1 = ctk.CTkButton(master=self.map_controls_frame, text="Add Marker 1", command=self.add_marker1_event)
+        self.button_add_marker1.pack(pady=(1, 5), padx=(10, 10))
+
+        self.label_marker2 = ctk.CTkLabel(master=self.map_controls_frame, text="Marker 2:", anchor="w")
+        self.label_marker2.pack(pady=(5, 0), padx=(10, 10))
+        self.entry_marker2 = ctk.CTkEntry(master=self.map_controls_frame, placeholder_text="Paste your coords...")
+        self.entry_marker2.pack(pady=(5, 0), padx=(10, 10))
+        self.button_add_marker2 = ctk.CTkButton(master=self.map_controls_frame, text="Add Marker 2", command=self.add_marker2_event)
+        self.button_add_marker2.pack(pady=(1, 10), padx=(10, 10))
+
+        self.button_clear_markers = ctk.CTkButton(master=self.map_controls_frame, text="Clear Markers", command=self.clear_marker_event)
+        self.button_clear_markers.pack(pady=(5, 10), padx=(10, 10))
+
+        self.map_label = ctk.CTkLabel(self.map_controls_frame, text="Tile Server:", anchor="w")
+        self.map_label.pack(pady=(1, 0), padx=(10, 10))
+        self.map_option_menu = ctk.CTkOptionMenu(self.map_controls_frame, values=["OpenStreetMap", "Google normal", "Google satellite"], command=self.change_map)
+        self.map_option_menu.pack(pady=(1, 5), padx=(10, 10))
+        self.distance_label = ctk.CTkLabel(self.map_controls_frame, text="Distance: ", anchor="w")
+        self.distance_label.pack(pady=(5, 35), padx=(10, 10))
+        self.map_option_menu.set("OpenStreetMap")
+        
+ # GRABBING CORDS FOR THE MAP WIDGET CAN BE TURNED INTO AN AUTO FUNCTION ===========================
+ 
+    def add_marker1_event(self):
+        coords = self.entry_marker1.get().strip().split()
+        if len(coords) == 2:
+            self.add_marker(1, float(coords[0]), float(coords[1]))
+
+    def add_marker2_event(self):
+        coords = self.entry_marker2.get().strip().split()
+        if len(coords) == 2:
+            self.add_marker(2, float(coords[0]), float(coords[1]))
+
+    def add_marker(self, marker_number, lat, lon):
+        marker = self.map_widget.set_marker(lat, lon, text=f"Marker {marker_number}")
+        self.marker_list.append(marker)
+        if len(self.marker_list) == 2:
+            self.calculate_distance()
+
+    def clear_marker_event(self):
+        self.map_widget.delete_all_marker()
+        self.marker_list.clear()
+        self.distance_label.configure(text="Distance: ")
+        
+# DISTANCE CALCS ===================================================================================
+
+    def calculate_distance(self):
+        if len(self.marker_list) == 2:
+            coords_1 = self.marker_list[0].position
+            coords_2 = self.marker_list[1].position
+            distance = geodesic(coords_1, coords_2).meters
+            self.distance_label.configure(text=f"Distance: {distance:.2f} meters")
+
+    def change_map(self, map_type):
+        tile_servers = {
+            "OpenStreetMap": "https://a.tile.openstreetmap.org/{z}/{x}/{y}.png",
+            "Google normal": "https://mt1.google.com/vt/lyrs=r&x={x}&y={y}&z={z}",
+            "Google satellite": "https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}",
+        }
+        self.map_widget.set_tile_server(tile_servers[map_type]) 
+        
+# NAVAGATIONAL BUTTONS CAN BE ADJUSTED =============================================================
+
+    def create_nav_buttons(self):
+        self.nav_buttons_frame = ctk.CTkFrame(master=self.frame, height=50, corner_radius=0)
+        self.nav_buttons_frame.pack(side="bottom", fill="x")
+
+        for btn_text, command in [("MAIN", self.switch_to_webcam), ("MAP", self.switch_to_map), 
+                                  ("STAT", self.switch_to_stat), ("SNAP", lambda: print("SNAP clicked")), 
+                                  ("SETT", lambda: print("SETT clicked")), ("QUIT", self.on_closing)]:
+            self.create_nav_button(btn_text, command)
+
+    def create_nav_button(self, text, command):
+        button = ctk.CTkButton(master=self.nav_buttons_frame, text=text, command=command, width=70, height=30)
+        button.pack(side="left", padx=6, pady=4)
+        
+# WEBCAM SETUP ====================================================================================
+
+    def start_webcam_thread(self):
+        self.thread = threading.Thread(target=self.show_webcam)
         self.thread.daemon = True
         self.thread.start()
+
+    def show_webcam(self):
+        cap = cv2.VideoCapture(0) # CHANGE VALUE IF WEBCAM IS NOT SHOWING ==============
+        while cap.isOpened():
+            ret, frame = cap.read()
+            if ret: # CROSSHAIR ========================================================
+                height, width, _ = frame.shape
+                cv2.line(frame, (width // 2 - 8, height // 2), (width // 2 + 8, height // 2), (0, 255, 0), 2)
+                cv2.line(frame, (width // 2, height // 2 - 8), (width // 2, height // 2 + 8), (0, 255, 0), 2)
+                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                frame = cv2.resize(frame, (580, 350))
+                img = Image.fromarray(frame)
+                imgtk = ImageTk.PhotoImage(image=img)
+                self.webcam_label.configure(image=imgtk)
+                self.webcam_label.image = imgtk
+            if cv2.waitKey(1) == ord('q'):
+                break
+        cap.release()
         
-        
-    def update(self): #ensures camera updates so no frame drops
-        while True:
-            if self.capture.isOpened():
-                (self.status, self.frame) = self.capture.read()
-            time.sleep(self.FPS)
-    
-    
-    def detect_and_show_objects(self):
-        if hasattr(self, 'frame'):
-            
-            # Increase sensitivity of detection by confidence %        vvvvvvvvvv     object trainer vvv  makes my pc go vroom
-            bbox, label, conf = cv.detect_common_objects(self.frame, confidence=0.35, model='yolov4-tiny')
-            
-            # Filter only 'bottle' objects for testing purposes because they are "plentiful on my desk"
-            bottle_indices = [i for i, lbl in enumerate(label) if lbl == 'bottle']
-            filtered_bbox = [bbox[i] for i in bottle_indices]
-            filtered_label = [label[i] for i in bottle_indices]
-            filtered_conf = [conf[i] for i in bottle_indices]
-            
-            # Draw bounding boxes and crosshair, considering adding distance measurements but then we got to teach this thing how to count, no thanks.
-            output_image, num_bottles = self.draw_bboxes_with_crosshair(self.frame, filtered_bbox, filtered_label, filtered_conf)
-            
-            # Add text overlay for the number of bottles displayed on the screen (planned on adding 99 bottles of beer joke :D )
-            detect_text = f'# of bottles: {num_bottles}'
-            #                                       x    y                            size    color  
-            cv2.putText(output_image, detect_text, (10, 30), cv2.FONT_HERSHEY_DUPLEX, 0.5, (255, 0, 0), 1, cv2.LINE_AA)
+#  TOGGLE SWITCHING BETWEEN DIFFERENT SCREENS IN THE UI ===========================================
 
-            accHD_text = f'Acceleration Data:'
-            cv2.putText(output_image, accHD_text, (10, 60), cv2.FONT_HERSHEY_DUPLEX, 0.6, (0, 255, 0), 1, cv2.LINE_AA)
-            
-            accx_text = f'X: [x value]'
-            cv2.putText(output_image, accx_text, (10, 80), cv2.FONT_HERSHEY_DUPLEX, 0.5, (0, 255, 0), 1, cv2.LINE_AA)
-            
-            accy_text = f'Y: [y value]'
-            cv2.putText(output_image, accy_text, (10, 100), cv2.FONT_HERSHEY_DUPLEX, 0.5, (0, 255, 0), 1, cv2.LINE_AA)
-            
-            accz_text = f'Z: [z value]'
-            cv2.putText(output_image, accz_text, (10, 120), cv2.FONT_HERSHEY_DUPLEX, 0.5, (0, 255, 0), 1, cv2.LINE_AA)
-            
-            floor_text = f'Distance from Floor: [#]in'
-            cv2.putText(output_image, floor_text, (10, 160), cv2.FONT_HERSHEY_DUPLEX, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
+    def switch_to_webcam(self):
+        self.map_widget.place_forget()
+        self.map_controls_frame.place_forget()
+        self.webcam_label.pack()
 
-            
-            cv2.imshow('frame', output_image)
-            cv2.waitKey(self.FPS_MS)
-    
-    
-    def draw_bboxes_with_crosshair(self, frame, bbox, label, conf):
-        num_bottles = len(bbox)
-        for box, lbl in zip(bbox, label):
-            
-            # Converting label to string
-            lbl_str = str(lbl)
-            
-            # Calculate center coordinates of the box so the cross hair is accurate, also is nice little hack
-            x_center = int((box[0] + box[2]) / 2)
-            y_center = int((box[1] + box[3]) / 2)
-            
-            # Draw bounding box
-            cv2.rectangle(frame, (box[0], box[1]), (box[2], box[3]), (0, 255, 0), 2)
-            
-            # Draw label on top of the bounding box AS A STRING!!!! can modify this to look fancy!
-            cv2.putText(frame, lbl_str, (box[0], box[1] - 10), cv2.FONT_HERSHEY_DUPLEX, 0.5, (255, 255, 255), 1)
-            
-            # Drawing crosshair at the center of the box from the lib 
-            cv2.drawMarker(frame, (x_center, y_center), (0, 0, 255), cv2.MARKER_CROSS, 10, 2)
-        
-        return frame, num_bottles # this thing counts bottles yay
+    def switch_to_map(self):
+        self.webcam_label.pack_forget()
+        self.map_controls_frame.place(x=0, y=0)
+        self.map_widget.place(x=0, y=0)
 
+    def switch_to_stat(self):
+        self.webcam_label.pack_forget()
+        self.map_widget.place_forget()
+        self.map_controls_frame.place_forget()
 
-# Example usage
+    def on_closing(self):
+        self.destroy()
+
 if __name__ == "__main__":
-    threaded_camera = ThreadedCamera(src=0)
-    while True:
-        threaded_camera.detect_and_show_objects()
-        if cv2.waitKey(1) & 0xFF == ord('q'): # this broke but i plan on doing something else down the line
-                                              # dont touch it, it brings me joy :v
-            break
+    app = App()
+    app.mainloop()
